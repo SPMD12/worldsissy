f54a658cfaa6a3cdd85dd38a25e4134fefc73a70
SPMD12 <76527584+SPMD12@users.noreply.github.com>
2021-02-12 18:14:09 +0300

    new

--- site.com/web/products.php
+++ site.com/web/products.php
@@ -3,23 +3,38 @@
 <html>
 <head>
 	<title>Мир SISSY девочек | SISSY Товары 18+ | worldsissy.com</title>
-  	<meta name="description" content="Мы делаем для SISSY девочек оригинальный авторский контент, которого не будет в общем доступе, покупая товары, вы нас очень поддерживаете.">
+	<meta name="description" content="Мы делаем для SISSY девочек оригинальный авторский контент, которого не будет в общем доступе, покупая товары, вы нас очень поддерживаете.">
 </head>
 <body>
 	<div class="wrapper">
-   		<div class="content">
-			<div class="card">
-				<img src="../img/journal.jpg" alt="" style="width:100%">
-				<p>Здесь можно купить журналы которые отвечают на многие ваши вопросы</p>
-				<a href="./journal.php">Журналы</a>
-			</div>
-			<div class="card">
-				<img src="../img/video.jpg" alt="" style="width:100%">
-				<p>Здесь можно купить приватные HYPNO-ВИДЕО которых нету в общем доступе</p>
-				<a href="./hypno-video.php">Hypno видео</a>
-			</div>
+		<div class="content">
+			<div class="cardWrapper">
+				<div class="card">
+					<div class="cardImg">
+						<img src="../img/journal.jpg">
+					</div>
+					<div class="cardText">
+						<p>Здесь можно купить журналы которые отвечают на многие ваши вопросы</p>
+					</div>
+					<div class="cardUrl">
+						<a href="./journal.php" class="glow-button"><strong>Журналы</strong></a>
+					</div>
+				</div>
+				<!-- Следующая карточка -->
+				<div class="card">
+					<div class="cardImg">
+						<img src="../img/video.jpg">
+					</div>
+					<div class="cardText">
+						<p>Здесь можно купить приватные HYPNO-ВИДЕО которых нету в общем доступе</p>
+					</div>
+					<div class="cardUrl">
+						<a href="./hypno-video.php" class="glow-button"><strong>Hypno видео</strong></a>
+					</div>
+				</div>
+			</div>		
 		</div>
-	 </div>
+	</div>
 <?php include ('../templates/footer.html.twig');?> 
 </body>
 </html>
